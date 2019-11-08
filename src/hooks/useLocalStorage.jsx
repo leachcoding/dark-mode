288bcad4d1e336911eae746ccdf1b9b8ebdc7f8a
Jay Leach <42565053+leachcoding@users.noreply.github.com>
2019-11-07 21:22:31 -0500

    Stretch and MVP finished.

---
+++ src/hooks/useLocalStorage.jsx
@@ -0,0 +1,17 @@
+import React, {useState} from 'react';
+
+const useLocalStorage = (key, initialValue) => {
+
+  const item = window.localStorage.getItem(key);
+
+  const [storedValue, setStoredValue] = useState(item || initialValue);
+
+  const setValue = value => {
+    window.localStorage.setItem(key, JSON.stringify(value));
+    setStoredValue(value);
+  }
+
+  return [storedValue, setValue];
+}
+
+export default useLocalStorage;
